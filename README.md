2741015f9c9f123c1c24f7a393806d8388323c10
MJ Rossetti <datacreativellc@gmail.com>
2018-04-07 20:19:42 -0400

    Prompt user to select a month, and parse selection

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 # Monthly Retail Sales Data
 
-## Usage
+## Installation
 
-### Generate a New Dataset
+```sh
+git clone git@github.com:prof-rossetti/monthly-retail-sales-data.git
+cd monthly-retail-sales-data
+bundle install
+```
+
+## Generating Data
+
+Run in development mode:
 
 ```sh
 ruby script/generate.rb
+```
+
+Run in test mode:
+
+```sh
+SCRIPT_ENV=test ruby script/generate.rb
 ```
--- script/generate.rb
+++ script/generate.rb
@@ -1,1 +1,26 @@
-puts "HELLO WORLD"
+require "pry"
+require "highline"
+require "csv"
+require "active_support/core_ext/date"
+require "active_support/core_ext/time"
+
+if ENV.fetch("SCRIPT_ENV") == "test"
+  response = "201803"
+else
+  cli = HighLine.new
+  response = cli.ask("Please enter a year and month (e.g. 201803)")
+end
+
+month_beg = Date.strptime(response, "%Y%m")
+puts "BEGIN #{month_beg.to_s}"
+month_end = month_beg.end_of_month
+puts "END #{month_end.to_s}"
+
+
+#binding.pry
+
+#CSV.open(csv_filepath, "w", :write_headers=> true, :headers => headers) do |csv|
+#  rankings.each do |ranking|
+#    csv << [1,2,3,4,5]
+#  end
+#end
